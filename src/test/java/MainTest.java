9afa7fc168e14ba084c041227212ca0e7aff0b72
lohade <abhisheklohade8584@gmail.com>
2021-10-20 20:22:34 +0530

    returnException when enter wrong file

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,15 +1,30 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class MainTest {
 
     String INDIA_CENSUS_PATH="C:\\Users\\Arti\\IdeaProjects\\IndianCensus\\src\\main\\resources\\Census.csv";
+    String STATE_DATA_PATH="C:\\Users\\Arti\\IdeaProjects\\IndianCensus\\src\\main\\resources\\IndianState.csv";
 
     @Test
-    public void checkfile_returntrue() throws CensusAnalyzerException {
+    public void checkfile_returntrue(){
+        try {
+            CensusAnalyzer censusAnalyzer=new CensusAnalyzer();
+            int numofrecords=censusAnalyzer.censusData(INDIA_CENSUS_PATH);
+            Assert.assertEquals(29,numofrecords);
 
-        CensusAnalyzer censusAnalyzer=new CensusAnalyzer();
-        int numofrecords=censusAnalyzer.censusData(INDIA_CENSUS_PATH);
-        Assert.assertEquals(29,numofrecords);
+        }catch (CensusAnalyzerException e){ }
+    }
+    @Test
+    public void ifIncorrectFile_returnException()  {
+        try {
+            CensusAnalyzer censusAnalyzer=new CensusAnalyzer();
+            ExpectedException exception=ExpectedException.none();
+            exception.expect(CensusAnalyzerException.class);
+            censusAnalyzer.censusData(STATE_DATA_PATH);
+        }catch (CensusAnalyzerException e){
+            Assert.assertEquals(CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
     }
 }
